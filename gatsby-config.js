8d4cba74c09077a6a43b1b843c8e7d9de3647da3
Babasato <114899243+Babasato@users.noreply.github.com>
2024-11-13 21:00:13 +0300

    Create gatsby-config.js

---
+++ gatsby-config.js
@@ -0,0 +1,12 @@
+module.exports = {
+  siteMetadata: {
+    title: "Travel Affiliate Site",
+    description: "A Gatsby site for travel affiliate marketing",
+    author: "Your Name"
+  },
+  plugins: [
+    "gatsby-plugin-react-helmet",
+    "gatsby-plugin-sharp",
+    "gatsby-transformer-sharp"
+  ]
+};
